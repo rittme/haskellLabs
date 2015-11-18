868bb49481197e9243a19c5eb4d3e7d9c32fa4ac
modou6c <contact.modoucisse@gmail.com>
2015-11-18 14:16:37 +0100

    operator <+ and properties; fullDeck and Draw

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -66,3 +66,50 @@ winner handGuest handBank | gameOver handGuest                 = Bank
                           | gameOver handBank                  = Guest
                           | value handGuest > value handBank   = Guest
                           | otherwise                          = Bank
+
+-- <+ : given 2 hands, puts the first on top of the second one
+(<+) :: Hand -> Hand -> Hand
+hand1 <+ Empty            = hand1
+Empty <+ hand2            = hand2
+(Add card hand1) <+ hand2 = (Add card (hand1 <+ hand2) )
+
+-- associative property of (<+) operator
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 = p1 <+ (p2 <+ p3) == (p1 <+ p2) <+ p3
+
+-- The size of the combined hand equals the sum of the 2 given hands
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf h1 h2 = size (h1 <+ h2) == size h1 + size h2
+
+-- fullDeck : returns a full deck of cards
+fullDeck :: Hand
+fullDeck =  (handBySuit Hearts)   <+ 
+            (handBySuit Spades)   <+ 
+            (handBySuit Diamonds) <+ 
+            (handBySuit Clubs)
+
+
+-- handBySuit: returns a hand consisting of all the cards in the given suit
+handBySuit :: Suit -> Hand
+handBySuit s =  (Add (Card Ace s) 
+                (Add (Card King s) 
+                (Add (Card Queen s) 
+                (Add (Card Jack s)
+                (Add (Card (Numeric 10) s)
+                (Add (Card (Numeric 9) s)
+                (Add (Card (Numeric 8) s)
+                (Add (Card (Numeric 7) s)
+                (Add (Card (Numeric 6) s)
+                (Add (Card (Numeric 5) s)
+                (Add (Card (Numeric 4) s)
+                (Add (Card (Numeric 3) s)
+                (Add (Card (Numeric 2) s)
+                Empty)))))))))))))
+
+-- Draw : Given a deck and a hand, 
+--        draw one card from the deck and put on the hand.
+draw :: Hand -> Hand -> (Hand, Hand)
+draw Empty hand             = error "draw: The deck is empty."
+draw (Add card hand1) hand2 = (hand1 , (Add card hand2))
+
+
