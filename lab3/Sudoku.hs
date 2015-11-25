e3043c3bedecf857f696acd2df6aff5c35b472f3
modou6c <contact.modoucisse@gmail.com>
2015-11-25 15:52:04 +0100

    part A

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -1,7 +1,19 @@
+{-
+  Functional Programming -- Lab Assignment 3 : Sudoku
+  ** Haskell program that can read in a Sudoku puzzle and solve it **
+
+  Group 38 - Bernardo Rittmeyer, Modou Cissé
+-}
+
 module Sudoku where
 
 import Test.QuickCheck
+import Data.Maybe
+import Data.List
 
+{- 
+  #A
+-}
 -------------------------------------------------------------------------
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
@@ -9,19 +21,77 @@ data Sudoku = Sudoku { rows :: [[Maybe Int]] }
 
 -- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
-allBlankSudoku = undefined
+allBlankSudoku = (Sudoku [allBlankRows | x<-[1..9]])
+      where allBlankRows = [Nothing | x<-[1..9]] 
+      --allBlankRows :: [Maybe Int]
 
 -- isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku = undefined
+isSudoku sudo = (isMatrixCorrect (rows sudo)) && (length (rows sudo)) == 9
+
+    where isMatrixCorrect :: [[Maybe Int]] -> Bool
+          isMatrixCorrect []        = True
+          isMatrixCorrect (row:xs)  = 
+                        (((hasSudokuValues row) && length row == 9)
+                        && (isMatrixCorrect xs))
+
+          hasSudokuValues :: [Maybe Int] -> Bool
+          hasSudokuValues row = and $ [True | x<-row, isValueOK x]
+
+          isValueOK :: Maybe Int -> Bool
+          isValueOK Nothing   = True
+          isValueOK x         = ((fromMaybe' x) > 0) || ((fromMaybe' x) < 10)
+
+          fromMaybe' :: Maybe a -> a -- to delete and use the original one ??
+          fromMaybe' (Just x) = x
+
 
 -- isSolved sud checks if sud is already solved, i.e. there are no blanks
 isSolved :: Sudoku -> Bool
-isSolved = undefined
+isSolved sudo = (hasOnlyNumValues (rows sudo))
 
+    where hasOnlyNumValues :: [[Maybe Int]] -> Bool
+          hasOnlyNumValues []       = True
+          hasOnlyNumValues (row:xs) = ((areNumValues row) && 
+                                      (hasOnlyNumValues xs))
+
+          areNumValues :: [Maybe Int] -> Bool
+          areNumValues rw = and $ [not (x == Nothing) | x<-rw]
+
+
+-- ******  TESTS --
+example :: Sudoku
+example =Sudoku
+      [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+      , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+      , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+      , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+      , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+      , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+      , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+      , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+      , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+      ]
+
+
+exampleFull :: Sudoku -- incorect
+exampleFull =Sudoku
+      [ [Just 3, Just 6, Just 8,Just 8,Just 7, Just 1, Just 2, Just 8,Just 8]
+      , [Just 8,Just 5, Just 8,Just 8,Just 8,Just 8,Just 1, Just 8, Just 8]
+      , [Just 8,Just 8,Just 9, Just 2, Just 8,Just 4, Just 7, Just 8,Just 8]
+      , [Just 8,Just 8,Just 8,Just 8,Just 1, Just 3, Just 8,Just 2, Just 8]
+      , [Just 4, Just 8,Just 8,Just 5, Just 8,Just 2, Just 8,Just 8,Just 9]
+      , [Just 2, Just 7, Just 8,Just 4, Just 6, Just 8,Just 8,Just 8,Just 8]
+      , [Just 8,Just 8,Just 5, Just 3, Just 8,Just 8, Just 9, Just 8,Just 8]
+      , [Just 8,Just 8, Just 3, Just 8,Just 8,Just 8,Just 8,Just 6, Just 8]
+      , [Just 8,Just 8,Just 7, Just 6, Just 9, Just 8,Just 8,Just 4, Just 3]
+      ]
 -------------------------------------------------------------------------
 
+{- 
+  #B
+-}
 -- printSudoku sud prints a representation of the sudoku sud on the screen
 printSudoku :: Sudoku -> IO ()
 printSudoku = undefined
